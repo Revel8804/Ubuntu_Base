0ed4c75fe9e74293919f1e2ea4d402d72d3dcfb3
Kenny <kenny@revelcomtech.com>
2020-05-28 14:54:29 +0000

    Added update script

--- updates.sh
+++ updates.sh
@@ -1,1 +1,13 @@
-test
+#!/bin/bash
+sudo apt clean
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+
+if [ -f /var/run/reboot-required ]; then
+  sudo shutdown -r now
+fi
+echo $(date) "Updates Complete" >> /scripts/logs/updates.log
+find -type f \( -name 'updates.log' \) -size +1M -delete
