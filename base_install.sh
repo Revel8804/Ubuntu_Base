5023271a393e063681213556efffc56f455bc921
Kenny <kenny@revelcomtech.com>
2020-05-29 17:36:17 +0000

    set updates script to run at end

--- base_install.sh
+++ base_install.sh
@@ -8,11 +8,12 @@ echo "$HOSTNAME" | tee /etc/hostname >/dev/null
 sed -i "s/template/${HOSTNAME}/g" /etc/hosts
 apt update
 apt install joe fail2ban open-vm-tools -y
-mkdir /scripts/
 mkdir /scripts/logs
 if test -f "$UPDATES"; then
 	rm -rf /scripts/updates.sh
 fi
 printf '#!/bin/bash\napt clean\napt udpate\napt upgrade -y\napt dist-upgrade -y\napt autoremove -y\napt autoclean -y\n\nif [ -f /var/run/reboot-required ]; then\n  sudo shutdown -r now\nfi\necho $(date) "Updates Complete" >> /scripts/logs/updates.log\nfind -type f \( -name 'updates.log' \) -size +1M -delete\n' >> /scripts/updates.sh
 chmod +x /scripts/updates.sh
+/scripts/updates.sh
+
 
