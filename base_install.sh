8465c49a6bd160d03b541569928960af8d796277
Kenny Bell <kenny@revel.local>
2020-06-03 14:31:24 -0500

    Fixed the slash on new lin

--- base_install.sh
+++ base_install.sh
@@ -12,7 +12,7 @@ mkdir /scripts/logs
 if test -f "$UPDATES"; then
 	rm -rf /scripts/updates.sh
 fi
-printf '#!/bin/bash\napt clean\napt update\napt upgrade -y\napt dist-upgrade -y\napt autoremove -y\napt autoclean -y\nif test -f "/scripts/base_install.sh"; then\n\trm "/scripts/base_install.sh"/nfi\nif [ -f /var/run/reboot-required ]; then\n  sudo shutdown -r now\nfi\necho $(date) "Updates Complete" >> /scripts/logs/updates.log\nfind -type f \( -name 'updates.log' \) -size +1M -delete\n' >> /scripts/updates.sh
+printf '#!/bin/bash\napt clean\napt update\napt upgrade -y\napt dist-upgrade -y\napt autoremove -y\napt autoclean -y\nif test -f "/scripts/base_install.sh"; then\n\trm "/scripts/base_install.sh"\nfi\nif [ -f /var/run/reboot-required ]; then\n  sudo shutdown -r now\nfi\necho $(date) "Updates Complete" >> /scripts/logs/updates.log\nfind -type f \( -name 'updates.log' \) -size +1M -delete\n' >> /scripts/updates.sh
 chmod +x /scripts/updates.sh
 /scripts/updates.sh
 
